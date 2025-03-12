342217be4c919f6160310c663ced084ae263189b
Irene Rubio <rubio.hernandez.irene@gmail.com>
2025-03-12 14:29:43 +0100

    third

--- pages/quizPage.js
+++ pages/quizPage.js
@@ -5,5 +5,9 @@ import { useContext } from 'react';
 export default function quizPage() {
   const { selectedQuiz } = useContext(QuizContext);
 
+  if (!selectedQuiz) {
+    return <div>No quiz selected</div>;
+  }
+
   return <QuizPage quiz={selectedQuiz} />;
 }
